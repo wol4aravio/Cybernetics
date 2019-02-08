0fac12b3d61268fc6c1f6657bd66d07f31769513
Valentin Panovskiy <wol4aravio@yandex.ru>
2019-02-08 16:45:31 +0300

    fix

--- cybernetics/controls/piecewise_constant_controller.py
+++ cybernetics/controls/piecewise_constant_controller.py
@@ -12,11 +12,11 @@ class PiecewiseConstantController:
         self._time_grid = list(time_grid) + [np.inf]
         self._values = values
 
-    def generate(self, tau: float) -> float:
-        for i, t in enumerate(self._time_grid[:-1]):
-            if t <= tau < self._time_grid[i + 1]:
+    def generate(self, t: float) -> float:
+        for i, t_prev in enumerate(self._time_grid[:-1]):
+            if t_prev <= t < self._time_grid[i + 1]:
                 return self._values[i]
-        raise ValueError(f"Can not generate control for tau <{tau}>")
+        raise ValueError(f"Can not generate control for tau <{t}>")
 
     def __call__(self, *args, **kwargs):
-        return self.generate(kwargs.get("tau", args[0]))
+        return self.generate(kwargs.get("t", args[0]))
